357a79bf826879977dfec0bf87e64a1e0ead2013
aaditya2290 <aaditya_2290@yahoo.co.in>
2020-02-25 00:24:45 +0530

    Refactored: Extracted logic for Tennis Game and Player creation to setUp method in test to ensure that tennis game has two players and also to avoid duplication.

--- src/test/java/com/bnpp/kata/tennisgame/TennisGameTest.java
+++ src/test/java/com/bnpp/kata/tennisgame/TennisGameTest.java
@@ -3,6 +3,7 @@ package com.bnpp.kata.tennisgame;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest {
@@ -11,27 +12,29 @@ public class TennisGameTest {
 	private static final String FIFTEEN_LOVE = "Fifteen Love";
 	private static final String LOVE_ALL = "Love All";
 	private TennisGame tennisGame;
+	private Player firstPlayer;
+	private Player secondPlayer;
+
+	@Before
+	public void setUp() {
+		firstPlayer = new Player();
+		secondPlayer = new Player();
+		tennisGame = new TennisGame(firstPlayer, secondPlayer);
+	}
 
 	@Test
 	public void scoreShouldBeLoveAllBeforeTennisGameBegins() {
-		tennisGame = new TennisGame();
 		assertThat(tennisGame.getScore(), is(LOVE_ALL));
 	}
 
 	@Test
 	public void scoreShouldBeFifteenLoveWhenFirstPlayerWinsOnePoint() {
-		Player firstPlayer = new Player();
-		Player secondPlayer = new Player();
-		tennisGame = new TennisGame(firstPlayer, secondPlayer);
 		firstPlayer.winsPoint();
 		assertThat(tennisGame.getScore(), is(FIFTEEN_LOVE));
 	}
 
 	@Test
 	public void scoreShouldBeLoveFifteenWhenSecondPlayerWinsOnePoint() {
-		Player firstPlayer = new Player();
-		Player secondPlayer = new Player();
-		tennisGame = new TennisGame(firstPlayer, secondPlayer);
 		secondPlayer.winsPoint();
 		assertThat(tennisGame.getScore(), is(LOVE_FIFTEEN));
 	}
