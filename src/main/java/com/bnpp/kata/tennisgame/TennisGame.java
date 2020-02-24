44a97e185d36c656486a3096f43f9cca2a313fcb
aaditya2290 <aaditya_2290@yahoo.co.in>
2020-02-25 04:38:43 +0530

    Refactored: Organised constants in business logic.

--- src/main/java/com/bnpp/kata/tennisgame/TennisGame.java
+++ src/main/java/com/bnpp/kata/tennisgame/TennisGame.java
@@ -2,14 +2,16 @@ package com.bnpp.kata.tennisgame;
 
 public class TennisGame {
 
-	private static final int POINTS_DIFFERENCE_FOR_WIN = 2;
-	private static final int POINTS_FOR_WIN = 4;
 	private static final String WINS = " Wins";
-	private static final int POINTS_DIFFERENCE_FOR_ADVANTAGE = 1;
 	private static final String ADVANTAGE = "Advantage ";
-	private static final int POINTS_FOR_DEUCE = 3;
 	private static final String DEUCE = "Deuce";
 	private static final String ALL = " All";
+
+	private static final int POINTS_DIFFERENCE_FOR_ADVANTAGE = 1;
+	private static final int POINTS_DIFFERENCE_FOR_WIN = 2;
+	private static final int POINTS_FOR_DEUCE = 3;
+	private static final int POINTS_FOR_WIN = 4;
+
 	private Player firstPlayer;
 	private Player secondPlayer;
 
